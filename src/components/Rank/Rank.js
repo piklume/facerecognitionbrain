4669de56722031331d2c88acdd45937d9875eae5
piklume <76256255+piklume@users.noreply.github.com>
2021-01-19 23:46:49 +0530

    first commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+       <div>
+          <div className='white f3'>
+            {'Sayak, your current rank is...'}
+          </div>
+          <div className='white f2'>
+            {'#1'}
+          </div>
+       </div>
+    );
+}
+
+export default Rank;
